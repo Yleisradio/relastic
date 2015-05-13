ff632f2b3e1af80fbaa57e4637154aa0f7d5d472
Tommi Haapaniemi <tommi.haapaniemi@reaktor.com>
2015-05-13 10:05:07 +0300

    Index creation

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [clojurewerkz/elastisch "2.1.0"]]
   :main ^:skip-aot relastic.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/relastic/core.clj
+++ src/relastic/core.clj
@@ -1,5 +1,16 @@
 (ns relastic.core
+(:require [clojurewerkz.elastisch.native.index :as esi]
+          [clojurewerkz.elastisch.native.document :as esd]
+          [clojurewerkz.elastisch.native :as elastisch])
   (:gen-class))
+
+(defn update-mapping [conn index version mappings settings & [map-fn]]
+  (let [index-name (str index "_v" version)]
+    (when-not (esi/exists? conn index-name)
+      (println "Index not found, creating...")
+      (esi/create conn index-name :mappings mappings :settings settings))))
+
+(def conn (elastisch/connect [["dockerhost" 9300]]))
 
 (defn -main
   "I don't do a whole lot ... yet."
