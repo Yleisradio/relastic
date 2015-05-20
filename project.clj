d561b4ef36b961139581860f8275c8815ac58f0a
Mikko Nylén <mnylen@reaktor.fi>
2015-05-20 11:46:12 +0300

    Update project information

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject relastic "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject yleisradio/relastic "0.1.0"
+  :description "ElasticSearch reindexing library"
+  :url "https://github.com/Yleisradio/new-reliquary"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
